8a3cbc89d2da1ac0340638390e134a6499f43a80
Ramil Zaynetdinov <ramil.zv@gmail.com>
2015-04-29 17:51:42 +0300

    Console: *: Rbac controller - add index, assign functions

--- commands/RbacController.php
+++ commands/RbacController.php
@@ -3,9 +3,17 @@ namespace app\commands;
 
 use Yii;
 use yii\console\Controller;
+use yii\helpers\Console;
+use app\modules\user\models\User;
 
 class RbacController extends Controller
 {
+    public function actionIndex()
+    {
+        echo 'yii rbac/init' . PHP_EOL;
+        echo 'yii rbac/assign' . PHP_EOL;
+    }
+
     public function actionInit()
     {
         $auth = Yii::$app->authManager;
@@ -22,16 +30,19 @@ class RbacController extends Controller
 
         // add "user" role
         $user = $auth->createRole('user');
+        $user->description = 'User';
         $auth->add($user);
         
         // add "author" role: create, update post
         $author = $auth->createRole('author');
+        $author->description = 'Author';
         $auth->add($author);
         $auth->addChild($author, $createPost);
         $auth->addChild($author, $updatePost);
 
         // add "admin" role: the permissions of the "user", "author" roles
         $admin = $auth->createRole('admin');
+        $admin->description = 'Administrator';
         $auth->add($admin);
         $auth->addChild($admin, $user);
         $auth->addChild($admin, $author);
@@ -42,5 +53,28 @@ class RbacController extends Controller
         $auth->assign($author, 2);
         $auth->assign($admin, 1);
         */
+    }
+
+    public function actionAssign()
+    {
+        $auth = Yii::$app->authManager;
+        $username = $this->prompt('Username:', ['required' => true]);
+        $role = $this->prompt('Role:', ['required' => true]);
+        $user = User::find()->where(['username' => $username])->one();
+        $role = $auth->getRole($role);
+        $this->log($auth->assign($role, $user->id));
+    }
+
+    /**
+     * @param bool $success
+     */
+    private function log($success)
+    {
+        if ($success) {
+            $this->stdout('Success!', Console::FG_GREEN, Console::BOLD);
+        } else {
+            $this->stderr('Error!', Console::FG_RED, Console::BOLD);
+        }
+        echo PHP_EOL;
     }
 }
