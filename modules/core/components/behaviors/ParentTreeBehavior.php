345cf363b7fd9c326ca829ed6b95a5106436b9a4
Ramil Zaynetdinov <ramil.zv@gmail.com>
2015-05-07 11:26:02 +0300

    Core Module v.0.0.1: *: ParentTreeBehavior - getItemsList rename to getParentsList

--- modules/core/components/behaviors/ParentTreeBehavior.php
+++ modules/core/components/behaviors/ParentTreeBehavior.php
@@ -7,20 +7,23 @@ use yii\helpers\ArrayHelper;
 class ParentTreeBehavior extends Behavior
 {
     public $parentAttr = 'parent_id';
+
     public $displayAttr = 'name';
+
+    public $status = 1;
 
     /**
      * @param bool $selfId
      * @return array
      */
-    public function getItemsList($selfId = false)
+    public function getParentsList($selfId = false)
     {
         $model = $this->owner;
         $condition = ($selfId)
             ? 'id != ' . $selfId
             : ''
         ;
-        $items = $model->find()->where($condition)->asArray()->all();
+        $items = $model->find()->where($condition)->andWhere(['status' => $this->status])->all();
 
         return ArrayHelper::map($items, 'id', $this->displayAttr);
     }
