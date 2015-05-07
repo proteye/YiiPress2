bc595de18727c236da3e2f179e1c2f7e7a028139
Ramil Zaynetdinov <ramil.zv@gmail.com>
2015-05-07 11:27:36 +0300

    Blog Module v.0.0.1: *: _form - add category dropDownList

--- modules/blog/views/post-backend/_form.php
+++ modules/blog/views/post-backend/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use app\modules\category\models\Category;
 
 /* @var $this yii\web\View */
 /* @var $model app\modules\blog\models\Post */
@@ -12,7 +13,7 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
 
-    <?= $form->field($model, 'category_id')->textInput() ?>
+    <?= $form->field($model, 'category_id')->dropDownList(Category::getItemsList(), ['prompt' => '-- нет --']) ?>
 
     <?= $form->field($model, 'lang')->textInput(['maxlength' => 2]) ?>
 
