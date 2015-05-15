c33fd2655797c5e8ecc22884c2187a04b0d7ed75
Ramil Zaynetdinov <ramil.zv@gmail.com>
2015-05-15 10:40:07 +0300

    Core Module v.0.0.4: *: core frontend controller - change actionRoute

--- modules/core/controllers/CoreFrontendController.php
+++ modules/core/controllers/CoreFrontendController.php
@@ -2,6 +2,7 @@
 
 namespace app\modules\core\controllers;
 
+use app\modules\blog\models\Tag;
 use Yii;
 use app\modules\core\components\controllers\FrontendController;
 use yii\filters\AccessControl;
@@ -57,6 +58,10 @@ class CoreFrontendController extends FrontendController
 
     public function actionRoute($url)
     {
+        /* Set current locale for Date */
+        $lang = str_replace('-', '_', Yii::$app->language) . '.UTF-8';
+        setlocale(LC_ALL, $lang);
+
         $id = false;
 
         /* Post */
@@ -76,11 +81,7 @@ class CoreFrontendController extends FrontendController
         if ($id !== false) {
             $model = Category::find()->where(['id' => $id, 'status' => Category::STATUS_ACTIVE])->one();
             if ($model) {
-                $posts = Post::find()->select(['category_id', 'title', 'slug', 'quote'])->where(['category_id' => $id, 'status' => Post::STATUS_ACTIVE])->asArray()->all();
-                foreach ($posts as $key => $val)
-                    $posts[$key]['url'] = Yii::$app->request->baseUrl . '/' . $pathsMap[$val['category_id']] . '/' . $val['slug'];
-
-                return $this->render('/blog/category', ['model' => $model, 'posts' => $posts]);
+                return $this->render('/blog/category', ['model' => $model]);
             }
         }
 
@@ -92,6 +93,16 @@ class CoreFrontendController extends FrontendController
             $model = Page::find()->where(['id' => $id, 'status' => Page::STATUS_ACTIVE])->one();
             if ($model)
                 return $this->render('/page/show', ['model' => $model]);
+        }
+
+        /* Tag */
+        $tagUrl = Yii::$app->getModule('blog')->tagUrl . '/';
+        $pos = strpos($url, $tagUrl);
+        if ($pos !== false) {
+            $tag = str_replace($tagUrl, '', $url);
+            $model = Tag::findOne(['slug' => $tag]);
+            if ($model)
+                return $this->render('/blog/tag', ['model' => $model]);
         }
 
         throw new \yii\web\HttpException(404, 'Страница не найдена.');
