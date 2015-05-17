5057306566e60cdd9f3cbf8a112213e5e903b956
Ramil Zaynetdinov <ramil.zv@gmail.com>
2015-05-17 23:28:29 +0300

    Core Module v.0.0.9: *: add thumbs, deleteImage

--- modules/core/components/behaviors/ImageUploadBehavior.php
+++ modules/core/components/behaviors/ImageUploadBehavior.php
@@ -10,6 +10,8 @@ class ImageUploadBehavior extends FileUploadBehavior
 
     public $path = 'image';
 
+    public $thumb_path = 'thumbs';
+
     public $resizeOnUpload = true;
 
     public $resizeOptions = [];
@@ -19,6 +21,14 @@ class ImageUploadBehavior extends FileUploadBehavior
         'height'  => 1200,
         'quality' => 75,
     ];
+
+    /**
+     * Before delete
+     */
+    public function beforeDelete()
+    {
+        $this->deleteImage();
+    }
 
     /**
      * @return string
@@ -45,8 +55,8 @@ class ImageUploadBehavior extends FileUploadBehavior
             return;
         }
 
-        /* Delete old file */
-        $this->deleteFile();
+        /* Delete old file and thumbs */
+        $this->deleteImage();
 
         /* Check and create target directory */
         if (!is_dir($this->uploadPath)) {
@@ -81,5 +91,62 @@ class ImageUploadBehavior extends FileUploadBehavior
 
         /* Set new filename to model->attribute */
         $model->{$this->attributeName} = $this->filename;
+    }
+
+    /**
+     * Delete image
+     */
+    protected function deleteImage()
+    {
+        $model = $this->owner;
+
+        if (!$old_filename = $model->getOldAttribute($this->attributeName)) {
+            return;
+        }
+
+        if (@is_file($this->getFilePath($old_filename))) {
+            @unlink($this->getFilePath($old_filename));
+            /* Delete all thumbs of this image */
+            $arr_filename = explode('.', $old_filename);
+            array_map('unlink', glob($this->thumbDirPath . DIRECTORY_SEPARATOR . $arr_filename[0] . '*'));
+        }
+    }
+
+    /**
+     * @return string
+     */
+    protected function getThumbDirPath()
+    {
+        return $this->getUploadPath() . DIRECTORY_SEPARATOR . $this->thumb_path;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getThumbDirUrl()
+    {
+        return $this->getUploadUrl() . '/' . $this->thumb_path;
+    }
+
+    /**
+     * @return string
+     */
+    public function getThumbUrl($width = 160, $height = 120, $quality = 75)
+    {
+        /* Thumb filename (ex. image_160_120.jpg) */
+        $arr_filename = explode('.', $this->owner->{$this->attributeName});
+        $filename = $arr_filename[0] . "_{$width}_{$height}." . $arr_filename[1];
+
+        if (!@is_file($this->thumbDirPath . DIRECTORY_SEPARATOR . $filename)) {
+            /* Check and create target directory */
+            if (!is_dir($this->thumbDirPath)) {
+                mkdir($this->thumbDirPath, 0755, true);
+            }
+            /* Create thumb */
+            Imagine::thumbnail($this->filePath, $width, $height)
+                ->save($this->thumbDirPath . DIRECTORY_SEPARATOR . $filename, ['quality' => $quality]);
+        }
+
+        return $this->getThumbDirUrl() . '/' . $filename;
     }
 }
