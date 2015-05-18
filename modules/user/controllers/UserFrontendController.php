405e04821c138da23e5dbae0ebded2a7a5b514cf
Ramil Zaynetdinov <ramil.zv@gmail.com>
2015-05-18 10:21:48 +0300

    User Module v.0.0.3: *: add behaviors

--- modules/user/controllers/UserFrontendController.php
+++ modules/user/controllers/UserFrontendController.php
@@ -6,6 +6,7 @@ use Yii;
 use app\modules\core\components\controllers\FrontendController;
 use yii\filters\VerbFilter;
 use app\modules\user\models\LoginForm;
+use yii\filters\AccessControl;
 
 /**
  * DefaultController implements the CRUD actions for User model.
@@ -15,9 +16,21 @@ class UserFrontendController extends FrontendController
     public function behaviors()
     {
         return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
+                    'logout' => ['post'],
                     'delete' => ['post'],
                 ],
             ],
