01c06682dd51ef3dd84c34201c2dc94b70cffdef
Ramil Zaynetdinov <ramil.zv@gmail.com>
2015-06-15 15:00:18 +0300

    *: config - add Google Adsense scripts

--- config/params.php
+++ config/params.php
@@ -1,7 +1,13 @@
 <?php
 
 return [
+    'meta_name' => '',
     'adminEmail' => '',
     'supportEmail' => '',
+    'fromEmail' => '',
+    'projectStartDate' => 2015,
+    'adsenseScript_top' => null,
+    'adsenseScript' => null,
+    'adsenseScript_bottom' => null,
     'user.passwordResetTokenExpire' => 3600,
 ];
