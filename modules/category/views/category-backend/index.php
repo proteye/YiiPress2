b0d38295c012be2dfa204e09ab01f96d6963cb24
Ramil Zaynetdinov <ramil.zv@gmail.com>
2015-08-28 13:40:07 +0300

    Category Module v.0.0.9: *: translate, add button

--- modules/category/views/category-backend/index.php
+++ modules/category/views/category-backend/index.php
@@ -7,7 +7,7 @@ use yii\grid\GridView;
 /* @var $searchModel app\modules\category\models\CategorySearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Categories';
+$this->title = 'Категории';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="category-index">
@@ -16,7 +16,8 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Category', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Добавить', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Типы категорий', ['/category/category-type-backend'], ['class' => 'btn btn-primary']) ?>
     </p>
 
     <?= GridView::widget([
--- modules/category/views/category-type-backend/index.php
+++ modules/category/views/category-type-backend/index.php
@@ -7,7 +7,7 @@ use yii\grid\GridView;
 /* @var $searchModel app\modules\category\models\CategoryTypeSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Category Types';
+$this->title = 'Типы категорий';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="category-type-index">
@@ -16,7 +16,8 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Category Type', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Добавить', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Категории', ['/category/category-backend'], ['class' => 'btn btn-warning']) ?>
     </p>
 
     <?= GridView::widget([
