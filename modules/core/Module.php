ff42669f109aacf345b377e1af51eeabe67da694
Ramil Zaynetdinov <ramil.zv@gmail.com>
2015-09-09 23:31:02 +0300

    Core Module v.0.2.8: *: bootstrap - add module url rules

--- modules/core/Module.php
+++ modules/core/Module.php
@@ -6,7 +6,7 @@ use Yii;
 
 class Module extends \yii\base\Module
 {
-    const VERSION = '0.2.7';
+    const VERSION = '0.2.8';
 
     public $controllerNamespace = 'app\modules\core\controllers';
 
--- modules/core/components/CoreBootstrap.php
+++ modules/core/components/CoreBootstrap.php
@@ -4,46 +4,51 @@ namespace app\modules\core\components;
 use Yii;
 use yii\base\BootstrapInterface;
 use yii\base\Application;
-use yii\caching\DbDependency;
 
 class CoreBootstrap implements BootstrapInterface
 {
     public $theme;
 
+    /**
+     * @param Application $app
+     * @return bool
+     * @throws \yii\base\InvalidConfigException
+     */
     public function bootstrap($app)
     {
-        $app->on(Application::EVENT_BEFORE_REQUEST, function () {
-            /* Set current THEME */
-            $theme = $this->theme;
+        /* Set current THEME */
+        $theme = $this->theme;
 
-            if (!$theme) {
-                /*
-                // Set cache
-                $duration = 60*60*24;
-                $dependency = new DbDependency(['sql' => 'SELECT MAX(updated_at) FROM {{%setting}}']);
-                $result = Yii::$app->db->cache(function ($db) {
-                    return Yii::$app->db->createCommand("SELECT param_value FROM {{%setting}} WHERE module_id='core' AND param_key = 'theme'")->queryOne();
-                }, $duration, $dependency);
-                */
-                $result = Yii::$app->db->createCommand("SELECT param_value FROM {{%setting}} WHERE module_id='core' AND param_key = 'theme'")->queryOne();
-                $theme = $result['param_value'];
-                if (!$theme)
-                    return false;
-            }
+        if (!$theme) {
+            /*
+            // Set cache
+            $duration = 60*60*24;
+            $dependency = new DbDependency(['sql' => 'SELECT MAX(updated_at) FROM {{%setting}}']);
+            $result = Yii::$app->db->cache(function ($db) {
+                return Yii::$app->db->createCommand("SELECT param_value FROM {{%setting}} WHERE module_id='core' AND param_key = 'theme'")->queryOne();
+            }, $duration, $dependency);
+            */
+            $result = Yii::$app->db->createCommand("SELECT param_value FROM {{%setting}} WHERE module_id='core' AND param_key = 'theme'")->queryOne();
+            $theme = $result['param_value'];
+            if (!$theme)
+                return false;
+        }
 
-            if (file_exists(Yii::getAlias('@app/themes/') . $theme)) {
-                Yii::setAlias('theme', '@app/themes/' . $theme);
-                Yii::$app->view->theme = Yii::createObject([
-                    'class' => '\yii\base\Theme',
-                    'basePath' => '@theme',
-                    'baseUrl' => '@web/themes/' . $theme,
-                    'pathMap' => [
-                        '@app/views' => '@theme',
-                        '@app/modules' => '@theme/modules',
-                    ],
-                ]);
-            }
-        });
+        if (file_exists(Yii::getAlias('@app/themes/') . $theme)) {
+            Yii::setAlias('theme', '@app/themes/' . $theme);
+            Yii::$app->view->theme = Yii::createObject([
+                'class' => '\yii\base\Theme',
+                'basePath' => '@theme',
+                'baseUrl' => '@web/themes/' . $theme,
+                'pathMap' => [
+                    '@app/views' => '@theme',
+                    '@app/modules' => '@theme/modules',
+                ],
+            ]);
+        }
+
+        /* Add module url rules */
+        $this->addUrlRules();
 
         /* Set default layouts for Mailer */
         Yii::$app->getMailer()->htmlLayout = '@core/mail/layouts/html';
@@ -52,5 +57,34 @@ class CoreBootstrap implements BootstrapInterface
         /* Set current locale for Date */
         $lang = str_replace('-', '_', Yii::$app->language) . '.UTF-8';
         setlocale(LC_TIME, $lang);
+    }
+
+    /**
+     * Add current module url rules
+     */
+    public function addUrlRules()
+    {
+        $moduleName = $this->_getModuleName();
+
+        if(Yii::$app->hasModule($moduleName))
+        {
+            $module = Yii::$app->getModule($moduleName);
+            if(isset($module->urlRules))
+            {
+                $urlManager = Yii::$app->getUrlManager();
+                $urlManager->addRules($module->urlRules);
+            }
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function _getModuleName()
+    {
+        $route = Yii::$app->request->pathInfo;
+        $domains = explode('/', $route);
+        $moduleName = array_shift($domains);
+        return $moduleName;
     }
 }
