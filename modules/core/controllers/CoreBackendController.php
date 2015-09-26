2e7e734eedad537ee4a15daae33f38ee69241175
Ramil Zaynetdinov <ramil.zv@gmail.com>
2015-09-26 23:50:55 +0300

    Core Module v.0.3.3: *: index - add log coupon csv import

--- modules/core/controllers/CoreBackendController.php
+++ modules/core/controllers/CoreBackendController.php
@@ -4,11 +4,24 @@ namespace app\modules\core\controllers;
 
 use Yii;
 use app\modules\core\components\controllers\BackendController;
+use app\modules\coupon\models\Coupon;
 
 class CoreBackendController extends BackendController
 {
     public function actionIndex()
     {
-        return $this->render('index');
+        $log_path = Yii::getAlias('@runtime') . DIRECTORY_SEPARATOR . 'logs' . DIRECTORY_SEPARATOR . Coupon::LOG_PATH;
+        $log_arr = null;
+        if (@is_file($log_path)) {
+            $fr = fopen($log_path, 'r');
+            $log_arr = fgetcsv($fr, 500, ';');
+            fclose($fr);
+            $fdate = date('d.m.Y H:i', filemtime($log_path));
+            array_push($log_arr, $fdate);
+        }
+
+        return $this->render('index', [
+            'log_arr' => $log_arr,
+        ]);
     }
 }
--- modules/core/views/core-backend/index.php
+++ modules/core/views/core-backend/index.php
@@ -1,9 +1,14 @@
 <?php
 /* @var $this yii\web\View */
 ?>
-<h1>core-backend/index</h1>
+<h1>Последние события</h1>
 
+<?php if ($log_arr): ?>
+<h2>Импорт купонов</h2>
 <p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
+    Дата: <?= $log_arr[3] ?><br/>
+    <?= $log_arr[0] ?><br/>
+    <?= $log_arr[1] ?><br/>
+    <?= $log_arr[2] ?><br/>
 </p>
+<?php endif; ?>
