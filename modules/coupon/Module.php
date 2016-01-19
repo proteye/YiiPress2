307661529f0e0dd9dc10bfebe09cc10ec069d72d
Ramil Zaynetdinov <me@proteye.ru>
2016-01-19 22:34:49 +0300

    Coupon Module v.0.3.1 *: import csv - equals begin and end dates

--- modules/coupon/Module.php
+++ modules/coupon/Module.php
@@ -4,7 +4,7 @@ namespace app\modules\coupon;
 
 class Module extends \yii\base\Module
 {
-    const VERSION = '0.3.0';
+    const VERSION = '0.3.1';
 
     public $controllerNamespace = 'app\modules\coupon\controllers';
 
