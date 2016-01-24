e6eec3411f07424a41d3f12a586810dc0e5125cb
Ramil Zaynetdinov <me@proteye.ru>
2016-01-24 23:16:31 +0300

    Core Module v.0.4.2 *: index page

--- modules/core/controllers/CoreBackendController.php
+++ modules/core/controllers/CoreBackendController.php
@@ -6,11 +6,36 @@ use Yii;
 use app\modules\core\components\controllers\BackendController;
 use app\modules\coupon\models\Coupon;
 
+/**
+ * Class CoreBackendController
+ * @package app\modules\core\controllers
+ */
 class CoreBackendController extends BackendController
 {
+    /**
+     * @return string
+     */
     public function actionIndex()
     {
-        $log_path = Yii::getAlias('@runtime') . D_S . 'logs' . D_S . Coupon::LOG_PATH;
+        /* Admitad */
+        $log_path = Yii::getAlias('@runtime') . D_S . 'logs' . D_S . Coupon::OFFER_ADMITAD . '_' . Coupon::LOG_PATH;
+        $log_admitad = self::logToArray($log_path);
+        /* Actionpay */
+        $log_path = Yii::getAlias('@runtime') . D_S . 'logs' . D_S . Coupon::OFFER_ACTIONPAY . '_' . Coupon::LOG_PATH;
+        $log_actionpay = self::logToArray($log_path);
+
+        return $this->render('index', [
+            'log_admitad' => $log_admitad,
+            'log_actionpay' => $log_actionpay,
+        ]);
+    }
+
+    /**
+     * @param $log_path
+     * @return array|null
+     */
+    private static function logToArray($log_path)
+    {
         $log_arr = null;
         if (@is_file($log_path)) {
             $fr = fopen($log_path, 'r');
@@ -20,9 +45,6 @@ class CoreBackendController extends BackendController
             $fdate = date('d.m.Y H:i', filemtime($log_path) + 10800); // UTC+3
             array_push($log_arr, $fdate);
         }
-
-        return $this->render('index', [
-            'log_arr' => $log_arr,
-        ]);
+        return $log_arr;
     }
 }
--- modules/core/views/core-backend/index.php
+++ modules/core/views/core-backend/index.php
@@ -3,12 +3,24 @@
 ?>
 <h1>Последние события</h1>
 
-<?php if ($log_arr): ?>
-<h2>Импорт купонов</h2>
+<?php if ($log_admitad): ?>
+<h2>Импорт купонов Admitad</h2>
 <p>
-    Дата: <?= $log_arr[3] ?><br/>
-    <?= $log_arr[2] ?><br/>
-    <?= $log_arr[1] ?><br/>
-    <?= $log_arr[0] ?><br/>
+    Дата: <?= $log_admitad[4] ?><br/>
+    <?= $log_admitad[3] ?><br/>
+    <?= $log_admitad[2] ?><br/>
+    <?= $log_admitad[1] ?><br/>
+    <?= $log_admitad[0] ?><br/>
 </p>
+<?php endif; ?>
+
+<?php if ($log_actionpay): ?>
+    <h2>Импорт купонов ActionPay</h2>
+    <p>
+        Дата: <?= $log_actionpay[4] ?><br/>
+        <?= $log_actionpay[3] ?><br/>
+        <?= $log_actionpay[2] ?><br/>
+        <?= $log_actionpay[1] ?><br/>
+        <?= $log_actionpay[0] ?><br/>
+    </p>
 <?php endif; ?>
