c8c9b72b90b1424440c0ece8287b10b310607095
Ramil Zaynetdinov <me@proteye.ru>
2016-02-27 00:50:19 +0300

    Core Module v.0.4.3 *: CoreModel - get upload dir

--- modules/core/models/CoreModel.php
+++ modules/core/models/CoreModel.php
@@ -8,9 +8,67 @@ use yii\db\ActiveRecord;
 
 class CoreModel extends ActiveRecord
 {
+    const THUMB_WIDTH = 320;
+    const THUMB_HEIGHT = 240;
+    const THUMB_QUALITY = 75;
+
     public static function find()
     {
         return new CoreQuery(get_called_class());
+    }
+
+    /**
+     * @return string
+     */
+    public static function getUploadDirUrl($module_id, $withHost = true)
+    {
+        $core = Yii::$app->getModule('core');
+        $module = Yii::$app->getModule($module_id);
+        $url = $core->uploadPath . '/' . $module->uploadPath;
+        if ($withHost) {
+            $url = Yii::$app->request->hostInfo . '/' . $url;
+        }
+        return $url;
+    }
+
+    /**
+     * @return string
+     */
+    public static function getThumbDirUrl($module_id, $withHost = true)
+    {
+        $core = Yii::$app->getModule('core');
+        $module = Yii::$app->getModule($module_id);
+        $url = $core->uploadPath . '/' . $module->uploadPath . '/thumbs';
+        if ($withHost) {
+            $url = Yii::$app->request->hostInfo . '/' . $url;
+        }
+        return $url;
+    }
+
+    /**
+     * @return string
+     */
+    public static function getPhotoUrl($module_id, $filename)
+    {
+        if (!$filename) {
+            return null;
+        }
+        return self::getUploadDirUrl($module_id) . '/' . $filename;
+    }
+
+    /**
+     * @return string
+     */
+    public static function getThumbUrl($module_id, $filename)
+    {
+        if (!$filename) {
+            return null;
+        }
+        $arr_filename[0] = substr($filename, 0, strrpos($filename, '.'));
+        $arr_filename[1] = substr($filename, strrpos($filename, '.'));
+        $thumb_filename = $arr_filename[0] . '_' . static::THUMB_WIDTH . '_' . static::THUMB_HEIGHT . $arr_filename[1];
+
+        return self::getThumbDirUrl($module_id) . '/' . $thumb_filename;
     }
 }
 
